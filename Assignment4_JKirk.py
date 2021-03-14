ad76820ddcb2a79259685b68e1581be2e7c53684
Jamey Kirk <jameykirk@gmail.com>
2021-03-14 12:02:44 +0900

    job done

--- Assignment4_JKirk.py
+++ Assignment4_JKirk.py
@@ -5,16 +5,23 @@ Date: 03/14/2021
 Description: program to identify a number is a prime number or not
 """
 
-def primeOrNot(value):
+def primeOrNot():
     """
     identify whether input number is a prime number or not
     """
-    pass
+    
+    again = True
+    while (again):
+        userNum = int(input("Please enter a number: "))
+        if (userNum / 2 % 2 == 1 or userNum / 2 % 2 == 0):
+            print(userNum, "is not a prime number")
+        else:
+            print(userNum, "is a prime number")
+        playAgain = input("Do you want to play again? (Y or N) ")
+        if (playAgain == "n" or playAgain == "N"):
+            again = False
+            print("\nThank you for playing.")
+        elif (playAgain == "y" or playAgain == "Y"):
+            again = True
 
-again = True
-
-while (again):
-    userNum = textinput("Please enter a number: ")
-    playAgain = textinput("Do you want to play again? (Y or N) ")
-    if (playAgain == "n" or playAgain == "N"):
-        again = False
+primeOrNot()
